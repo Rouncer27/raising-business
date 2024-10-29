f01eaaa157681ee7fb9e3e6f40702c8799172efa
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 15:55:00 -0600

    new form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,32 +1,74 @@
-import React from "react"
+import React, { useState } from "react"
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    email: "",
+    comments: "",
+  })
+
+  const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formData }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
+  const handleOnChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
   return (
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-      netlify
-      onSubmit="submit"
-    >
+    <form onSubmit={handleSubmit}>
       <input type="hidden" name="form-name" value="contact" />
       <div>
         <label>
           First Name <br />
-          <input type="text" name="first-name" />
+          <input
+            type="text"
+            name="first-name"
+            value={formData.firstName}
+            onChange={e => handleOnChange(e)}
+          />
         </label>
       </div>
 
       <div>
         <label htmlFor="email">Email</label>
         <br />
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={e => handleOnChange(e)}
+        />
       </div>
 
       <div>
         <label>
           Any Comments <br />
-          <textarea name="comments" rows="5" cols="40"></textarea>
+          <textarea
+            name="comments"
+            rows="5"
+            cols="40"
+            value={formData.comments}
+            onChange={e => handleOnChange(e)}
+          ></textarea>
         </label>
       </div>
 
