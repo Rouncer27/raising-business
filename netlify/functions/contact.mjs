450d2ec7a996167c77dc3b982d3d0ba4f8c3b7da
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 19:38:48 -0600

    added requiremnt for fields

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -1,4 +1,6 @@
-exports.handler = async function () {
+exports.handler = async function (req, context) {
+  console.log("req: ", req)
+  console.log("context: ", context)
   return {
     statusCode: 200,
     body: JSON.stringify({
