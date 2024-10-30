5a6c131a89cd4fe96072e9e214a8b11b973b8527
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 19:26:26 -0600

    added requiremnt for fields

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,11 +1,8 @@
-exports.handler = async (event, context) => {
-  console.log("event: ", event)
-  console.log("contextevent: ", context)
-  const { email } = JSON.parse(event.body)
-  console.log("email: ", email)
-
+exports.handler = async function () {
   return {
     statusCode: 200,
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({
+      message: "That was easy!",
+    }),
   }
 }
