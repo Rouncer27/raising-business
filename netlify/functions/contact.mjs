fccdaa641a66393cfd13c5d9dbed7beccb8cd177
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 19:59:44 -0600

    added requiremnt for fields

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -1,20 +1,18 @@
 exports.handler = async function (req, context) {
   console.log("req: ", req.body)
-  const formData = req.body
-  console.log("formData: ", formData)
-
-  const encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&")
-  }
-
   const FORM_POST_URL = `https://rab.swbcreative.ca/wp-json/contact-form-7/v1/contact-forms/165/feedback`
+  const formData = req.body
+
+  const formDataArray = Object.entries(formData)
+  const bodyFormData = new FormData()
+  formDataArray.forEach(field => {
+    bodyFormData.append(field[0], field[1])
+  })
 
   const response = await fetch(FORM_POST_URL, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({ ...formData }),
+    body: bodyFormData,
   })
 
   console.log("response: ", response)
