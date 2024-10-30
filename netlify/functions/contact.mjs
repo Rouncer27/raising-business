74859cce3f631bd0e1da2146c4a0b37984f22ae5
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 21:41:00 -0600

    form

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -18,6 +18,8 @@ exports.handler = async function (req, context) {
 
   return {
     statusCode: response.status,
-    body: JSON.stringify( response.data ),
+    body: JSON.stringify({
+      data: response.data
+    }),
   }
 }
