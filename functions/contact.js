bbfcfb191531590f49dfb9f77bdb5ce6b741ea52
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 19:15:49 -0600

    added requiremnt for fields

--- functions/contact.js
+++ functions/contact.js
@@ -3,4 +3,9 @@ exports.handler = async (event, context) => {
   console.log("contextevent: ", context)
   const { email } = JSON.parse(event.body)
   console.log("email: ", email)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ email }),
+  }
 }
