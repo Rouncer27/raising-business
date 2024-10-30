f18481786bc4f2dfbfe33a10e3c086c58e92e228
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 20:36:11 -0600

    form

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -1,7 +1,10 @@
+import axios from "axios"
+
 exports.handler = async function (req, context) {
   console.log("req Type:  ", typeof req.body)
   console.log("req : ", req.body)
   const FORM_POST_URL = `https://rab.swbcreative.ca/wp-json/contact-form-7/v1/contact-forms/165/feedback`
+  const config = { headers: { "Content-Type": "multipart/form-data" } }
   const formData = JSON.parse(req.body)
 
   console.log("formData: ", formData)
@@ -12,11 +15,13 @@ exports.handler = async function (req, context) {
     bodyFormData.append(field[0], field[1])
   })
 
-  const response = await fetch(FORM_POST_URL, {
-    method: "POST",
-    headers: { "Content-Type": "multipart/form-data" },
-    body: bodyFormData,
-  })
+  const response = await axios.post(FORM_POST_URL, formData, config)
+
+  // const response = await fetch(FORM_POST_URL, {
+  //   method: "POST",
+  //   headers: { "Content-Type": "multipart/form-data" },
+  //   body: bodyFormData,
+  // })
 
   console.log("response: ", response)
 
